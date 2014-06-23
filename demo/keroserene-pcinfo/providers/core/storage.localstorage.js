2beee8ef4009c9f761a1f19e8f909973a2cff6bb
Will Scott <willscott@gmail.com>
2014-06-23 11:23:22 -0700

    https://github.com/freedomjs/freedom/commit/276998adc1a8523a91ef8aaf1ab25bb3be75fd66

---
+++ demo/keroserene-pcinfo/providers/core/storage.localstorage.js
@@ -0,0 +1,77 @@
+/*globals fdom, localStorage */
+/*jslint indent:2,sloppy:true */
+/**
+ * A FreeDOM core.storage provider that depends on localStorage
+ * Thus, this only works in the context of a webpage and has
+ * some size limitations.
+ * Note that this can conflict with other scripts using localStorage
+ * as keys are raw
+ * @Class Storage_unprivileged
+ * @constructor
+ * @private
+ * @param {App} app The application creating this provider.
+ */
+var Storage_unprivileged = function (app, dispatchEvent) {
+  this.app = app;
+  fdom.util.handleEvents(this);
+};
+
+/**
+ * Lists keys in the storage repository
+ * @method keys
+ */
+Storage_unprivileged.prototype.keys = function (continuation) {
+  var result = [],
+    i;
+  for (i = 0; i < localStorage.length; i += 1) {
+    result.push(localStorage.key(i));
+  }
+  continuation(result);
+};
+
+/**
+ * Get a key from the storage repository.
+ * @param {String} key The item to get from storage.
+ * @method get
+ */
+Storage_unprivileged.prototype.get = function (key, continuation) {
+  try {
+    var val = localStorage.getItem(key);
+    continuation(val);
+  } catch (e) {
+    continuation(null);
+  }
+};
+
+/**
+ * Set a key in the storage repository.
+ * @param {String} key The item to save in storage.
+ * @param {String} value The value to save in storage.
+ * @method set
+ */
+Storage_unprivileged.prototype.set = function (key, value, continuation) {
+  localStorage.setItem(key, value);
+  continuation();
+};
+
+/**
+ * Remove a key from the storage repository.
+ * @param {String} key The item to remove from storage;
+ * @method remove
+ */
+Storage_unprivileged.prototype.remove = function (key, continuation) {
+  localStorage.removeItem(key);
+  continuation();
+};
+
+/**
+ * Reset the contents of the storage repository.
+ * @method clear
+ */
+Storage_unprivileged.prototype.clear = function (continuation) {
+  localStorage.clear();
+  continuation();
+};
+
+/** REGISTER PROVIDER **/
+fdom.apis.register("core.storage", Storage_unprivileged);
