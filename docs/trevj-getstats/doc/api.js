727cde0e0bfeed7a21c0cbf4520557202fefd03d
Will Scott <willscott@gmail.com>
2014-06-25 13:57:38 -0700

    https://github.com/freedomjs/freedom/commit/58b8362130079a5a0d72e5ed88cd82b502508929

---
+++ docs/trevj-getstats/doc/api.js
@@ -0,0 +1,30 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "API",
+        "Hub",
+        "Link",
+        "Link.Direct",
+        "Manager",
+        "Module",
+        "ModuleInternal",
+        "Policy",
+        "Provider",
+        "Proxy",
+        "Resource",
+        "Worker",
+        "handleEvents",
+        "link.Frame",
+        "util"
+    ],
+    "modules": [
+        "freedom"
+    ],
+    "allModules": [
+        {
+            "displayName": "freedom",
+            "name": "freedom"
+        }
+    ]
+} };
+});
