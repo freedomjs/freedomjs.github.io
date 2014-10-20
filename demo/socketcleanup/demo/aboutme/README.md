d18a5dec712f1803c74c5912b0bbb9a9bed1af60
Raymond Cheng <ryscheng@cs.washington.edu>
2014-10-20 16:02:40 -0400

    removing deleted branches

--- demo/socketcleanup/demo/aboutme/README.md
+++
@@ -1,6 +0,0 @@
-About Me
-======
-
-A profile example, which make use of the identity API to display
-a portfolio page for the user, showing their information and
-friendship information that is available to freedom applications.
--- demo/socketcleanup/demo/aboutme/aboutme.js
+++
@@ -1,9 +0,0 @@
-var identity = freedom.identity();
-
-// deferring execution in a setTimeout produces more comprehensible debugging.
-setTimeout(function() {
-  var promise = identity.login({});
-  promise.done(function(data) {
-    freedom.emit('profile', data);
-  });
-}, 0);
