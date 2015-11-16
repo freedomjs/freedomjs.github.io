87c92bc5db12d16a51c7593b892af9c7d3f1b121
soycode <soycode@users.noreply.github.com>
2015-11-16 10:06:18 -0800

    https://github.com/freedomjs/freedom/commit/24b46fe08c86f03eb0357afbfeb877dc4f06f197

---
+++ dist/freedom/v0.6.27/doc/api.js
@@ -0,0 +1,25 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "API",
+        "Consumer",
+        "Direct",
+        "Frame",
+        "Hub",
+        "Link",
+        "Manager",
+        "Module",
+        "ModuleInternal",
+        "Policy",
+        "Provider",
+        "ProxyBinder",
+        "Resource",
+        "Worker",
+        "handleEvents",
+        "util"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
